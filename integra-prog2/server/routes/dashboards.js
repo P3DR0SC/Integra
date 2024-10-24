31c2dee450352d4b0bd319277db356505596be65
pedro999924 <sanchesyucuma007@gmail.com>
2024-10-24 18:43:51 -0300

    Celesc eres homo

--- integra-prog2/server/routes/dashboards.js
+++ integra-prog2/server/routes/dashboards.js
@@ -3,11 +3,20 @@ const router = express.Router();
 router.use(express.json());
 
 
-router.post("/$nome", (req, res) => {
-    console.log('Você é um {$perm}'); 
+router.post("/assinante", (req, res) => {
+    console.log('Você é um Assiannte'); 
    
 });
 
+router.post("/adm", (req, res) => {
+    console.log('Você é um adm'); 
+   
+});
+
+router.post("/professor", (req, res) => {
+    console.log('Você é um professor'); 
+   
+});
 
 
 module.exports = router;
