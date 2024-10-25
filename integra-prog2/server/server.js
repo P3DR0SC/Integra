197b52c7d84c6125ec3fdaf26deacafb9fc4aafc
Pedro <pedro.augusto@tech4log.com>
2024-10-25 08:18:03 -0300

    server.js mais rotas

--- integra-prog2/server/server.js
+++ integra-prog2/server/server.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
-
+app.use(express.json());
 const usuario =require("./routes/usuario");
 app.use("/usuario", usuario);
+
+const login =require("./routes/login");
+app.use("/login", login);
+
+const dashboard =require("./routes/dashboard");
+app.use("/dashboard", dashboard);
+
+const cadastro =require("./routes/cadastro");
+app.use("/cadastro", cadastro);
 
 // Defina suas rotas da API
 app.get('/home', (req, res) => {
   res.json({ message: "Hello from the server!" });
 });
 
-const professor =require("./routes/professor");
-app.use("/professor", professor);
-
-const professor =require("./routes/");
-app.use("/professor", professor);
-
-
 
 // Inicia o servidor
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("Server is running on port ${PORT}");
 });
